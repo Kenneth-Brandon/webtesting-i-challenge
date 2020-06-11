c0cb2a3911475c54b37136c111c43eb98748545e
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-06-10 21:44:09 -0700

    MVP and some stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,32 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  } else {
+    item.durability = item.durability - 10;
+  }
+  if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
+  if (item.enhancement > 0) {
+    item.name = `[+${item.enhancement}] ${item.name}`;
+  }
   return { ...item };
 }
